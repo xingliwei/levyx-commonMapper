9770dbe654724c69737527243823448cda82cfb2
xingliwei <362493656@qq.com>
2016-09-27 14:29:45 +0800

    commonMapper project init

---
+++ src/main/java/com/xlw/levyx/mapper/model/User.java
@@ -0,0 +1,75 @@
+package com.xlw.levyx.mapper.model;
+
+import java.util.Date;
+
+public class User {
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+    private String password;
+
+    private String nickname;
+
+    private Date createtime;
+
+    private Date updatetime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname == null ? null : nickname.trim();
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public Date getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(Date updatetime) {
+        this.updatetime = updatetime;
+    }
+}
