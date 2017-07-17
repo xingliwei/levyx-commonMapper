5f50b6dc308be1931041be841b79276c65bbb07b
xingliwei <362493656@qq.com>
2017-07-17 14:05:33 +0800

    添加数据库脚本

---
+++ src/main/db/levyx_common_templete.sql
@@ -0,0 +1,67 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50711
+Source Host           : localhost:3306
+Source Database       : levyx_common_templete
+
+Target Server Type    : MYSQL
+Target Server Version : 50711
+File Encoding         : 65001
+
+Date: 2017-07-17 14:04:39
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for base_student
+-- ----------------------------
+DROP TABLE IF EXISTS `base_student`;
+CREATE TABLE `base_student` (
+  `id` varchar(64) NOT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `gender` int(11) DEFAULT NULL,
+  `hobby` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='学生表';
+
+-- ----------------------------
+-- Table structure for base_user
+-- ----------------------------
+DROP TABLE IF EXISTS `base_user`;
+CREATE TABLE `base_user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `nick_name` varchar(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for student_score
+-- ----------------------------
+DROP TABLE IF EXISTS `student_score`;
+CREATE TABLE `student_score` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` varchar(64) DEFAULT NULL,
+  `socre` int(11) DEFAULT NULL,
+  `subject_id` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='学生成绩表';
+
+-- ----------------------------
+-- Table structure for student_subject
+-- ----------------------------
+DROP TABLE IF EXISTS `student_subject`;
+CREATE TABLE `student_subject` (
+  `id` varchar(64) NOT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='学生课程';
